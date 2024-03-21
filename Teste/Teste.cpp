18a663127187a1920ee2d29dc83449534accf9c7
Guilherme Michel <139629718+Gui0r@users.noreply.github.com>
2024-03-20 21:44:19 -0300

    Adicionando arquivos

---
+++ Teste/Teste.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int TextoFoda(){
+     std::cout << "Meu nome é Guilherme e eu tenho 16 anos" << std::endl;
+    
+    std::cout << "Bombou!" << std::endl;
+    return 0;
+}
+
+int main(){
+    TextoFoda();
+/*
+    printf("Meu nome é Guilherme e eu tenho 16 anos,\n");
+    printf("Bombou!\n");
+*/ 
+/*
+    std::cout << "Meu nome é Guilherme e eu tenho 16 anos" << std::endl;
+    
+    std::cout << "Bombou!" << std::endl;
+    return 0;
+*/
+}
